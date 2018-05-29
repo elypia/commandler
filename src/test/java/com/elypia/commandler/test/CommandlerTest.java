0a3e508728a49b814390f641ff667292331c3f50
Syed Seth <seth@elypia.com>
2018-05-29 13:26:48 +0100

    - (Was using Junit 4 instead of Junit 5 by mistake)

--- src/test/java/com/elypia/commandler/test/CommandlerTest.java
+++ src/test/java/com/elypia/commandler/test/CommandlerTest.java
@@ -1,11 +1,11 @@
 package com.elypia.commandler.test;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class CommandlerTest {
 
     @Test
     public void tempTest() {
-        
+
     }
 }
