38f2402efcd0a55143f47cc2a8a05bcb934feb89
Seth <sethsutopia@gmail.com>
2018-06-02 17:58:18 +0100

    - Removed prune command from example package - Updated JDA

--- src/example/java/com/elypia/commandlerbot/modules/GuildModule.java
+++ src/example/java/com/elypia/commandlerbot/modules/GuildModule.java
@@ -5,16 +5,11 @@ import com.elypia.commandler.annotations.Command;
 import com.elypia.commandler.annotations.CommandGroup;
 import com.elypia.commandler.annotations.Module;
 import com.elypia.commandler.annotations.Param;
-import com.elypia.commandler.annotations.access.Permissions;
 import com.elypia.commandler.annotations.access.Scope;
 import com.elypia.commandler.annotations.filter.Search;
-import com.elypia.commandler.annotations.validation.Limit;
 import com.elypia.commandler.events.MessageEvent;
 import net.dv8tion.jda.core.EmbedBuilder;
-import net.dv8tion.jda.core.Permission;
-import net.dv8tion.jda.core.entities.ChannelType;
 import net.dv8tion.jda.core.entities.Guild;
-import net.dv8tion.jda.core.entities.TextChannel;
 
 import static com.elypia.commandler.data.SearchScope.MUTUAL;
 import static net.dv8tion.jda.core.entities.ChannelType.PRIVATE;
@@ -45,27 +40,5 @@ public class GuildModule extends CommandHandler {
         builder.addField("Total Members", String.valueOf(guild.getMembers().size()), true);
 
         return builder;
-    }
-
-    @Permissions({Permission.MESSAGE_MANAGE, Permission.MESSAGE_HISTORY})
-    @Scope(ChannelType.TEXT)
-    @CommandGroup("prune")
-    @Command(aliases = "prune", help = "Delete messages in the guild in bulk.")
-    @Param(name = "count", help = "The number of messages to delete.")
-    public void prune(MessageEvent event, int count) {
-        prune(event, count, event.getMessageEvent().getTextChannel());
-    }
-
-    @Permissions({Permission.MESSAGE_MANAGE, Permission.MESSAGE_HISTORY})
-    @Scope(ChannelType.TEXT)
-    @CommandGroup("prune")
-    @Param(name = "count", help = "The number of messages to delete.")
-    @Param(name = "channel", help = "The channel to delete messages in.")
-    public void prune(MessageEvent event, @Limit(min = 2, max = 100) int count, TextChannel channel) {
-        channel.getHistoryBefore(event.getMessageEvent().getMessage(), count).queue(o -> {
-            channel.deleteMessages(o.getRetrievedHistory()).queue(ob -> {
-                event.tryDeleteMessage();
-            });
-        });
     }
 }
