5b465ddb67b8455169c883712cfd5bd7527824e6
Seth <sethsutopia@gmail.com>
2018-06-23 12:01:12 +0100

    - Changed the fix as I didn't like the previous - Also added extra step when checking if command is valid (compare to root aliases first to and see if the module is registered at all before actually trying to get the module)

--- src/main/java/com/elypia/commandler/events/MessageEvent.java
+++ src/main/java/com/elypia/commandler/events/MessageEvent.java
@@ -126,14 +126,7 @@ public class MessageEvent {
 			return;
 
 		module = matcher.group("module");
-
-		if (module != null)
-			module = module.toLowerCase();
-
 		command = matcher.group("commands");
-
-		if (command != null)
-			command = command.toLowerCase();
 
 		String parameters = matcher.group("params");
 
@@ -259,7 +252,7 @@ public class MessageEvent {
 	}
 
     /**
-     * @return Get the module of a commands, this is normally the first thing
+     * @return Get the module of a command, this is normally the first thing
      * after the prefix.
      */
 
