75fadb2f99f0648e65fd701c3af90cacefbe49e7
Syed Seth <seth@elypia.com>
2018-12-06 03:30:25 +0000

    - [skip-ci] This is a WIP commit, won't compile. Real final commit of the day! - Removed `normalize` from `ICommandProcessor` interface as users shouldn't be _required_ to implement that, depending on their implementation of `process` they may not need it.

--- core/src/main/java/com/elypia/commandler/interfaces/ICommandProcessor.java
+++ core/src/main/java/com/elypia/commandler/interfaces/ICommandProcessor.java
@@ -27,33 +27,4 @@ public interface ICommandProcessor<C, E, M> {
      * @return The input the user provided or null if it's not a valid command.
      */
     public CommandEvent process(Commandler<C, E, M> commandler, E event, String content);
-
-    /**
-     * If the command follows valid syntax, normalize the command as
-     * depending on if a {@link Default} or {@link Static} command
-     * was performed some of the components may need to be shifted
-     * or added.
-     * <strong>Note:</strong> This may not be required, it depends on the
-     * {@link IConfiler#processEvent(Commandler, Object, String)} implementation.
-     * The default implementation {@link Confiler} does require normalization. <br>
-     *
-     * During normalization the following it performed:
-     * <ul>
-     *     <li>
-     *         If the root alias or "module" found is a reference to a module
-     *         we select that module. If there is no command that follows, we look
-     *         for a default command, else raise an event. If we do find a command
-     *         we select that command and perform it.
-     *     </li>
-     *     <li>
-     *         If the root alias or "module" is a reference to a static command
-     *         we shift it to "command" and select the module that static command
-     *         belongs to, if there is a "command", we insert that as the first
-     *         parameter.
-     *     </li>
-     * </ul>
-     *
-     * @return If the command is still valid.
-     */
-    boolean normalize(ICommandEvent<C, E, M> event);
 }
